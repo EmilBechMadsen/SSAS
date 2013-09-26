de4ec9c464456122a170f7263d37c140b558e657
sualitu <sual@itu.dk>
2013-09-26 17:00:37 +0200

    Initial work on routing.

--- src/main/scala/dk/itu/ssas/Service.scala
+++ src/main/scala/dk/itu/ssas/Service.scala
@@ -23,6 +23,7 @@ class Service
 
   def actorRefFactory = context
   def receive = runRoute(route)
+  def cookieName : String = "ssas_session"
 
   val route = 
     path("test") {
@@ -31,5 +32,114 @@ class Service
           "Hello"
         }
       }
-    }
+    } ~
+    path("signup") {
+    	get {
+    		complete {
+    			""
+    		}
+    	} ~
+    	post {
+    		entity(as[String]) { email => 
+    			entity(as[String]) { password =>
+    				complete {
+    					email + password
+    				}
+    			}
+    		}
+    	}
+    } ~
+    path("confirm") { 
+    	entity(as[String]) {token =>
+	    	get {
+	    			complete {
+	    				token
+	    			}
+	    	} ~
+	    	post {
+	    		entity(as[String]) { password =>
+	    			complete {
+	    				password + token
+	    			}
+	    		}
+	    	}
+    	}
+    } ~
+    path("requests") {
+    	get {
+    		complete {
+    				""
+    		}
+  		} ~
+    	post {
+    		entity(as[String]) { accepted =>
+    			entity(as[String]) { relationship =>
+    				complete {
+		    			// SQL Injection Check
+		    			// Check User Token
+		    			""
+		    		}
+    			}
+    		}
+    	}	
+    } ~
+   	path("profile") {
+   		entity(as[String]) { id =>
+	   		get {
+	    			complete {
+	    				// SQL Injection Check
+	    				// Check User Token
+	    				// If id is user
+	    					// Get self profile page
+	    				// else
+	    					// If users are friends
+	    						// Get friend profile page
+	    					// else
+	    						// Reject
+	    				""
+	    			}
+	    	}
+    	}
+   	} ~
+   	path("friends") {
+   		get {
+    			complete {
+    				// SQL Injection Check
+    				// Check User Token
+    				// Get page with friends
+    				""
+    			}
+    	}
+   	} ~
+   	path("search") {
+   		post {			
+   			entity(as[String]) { search =>
+    			complete {
+    				// SQL Injeciton Check
+    				// Check User Token
+    				// Get page for search criteria
+    				""
+    			}
+	    	}
+    	}
+   	}
+   	path("login") {
+   		post {
+   			entity(as[String]) { email => 
+    			entity(as[String]) { password =>
+    				// SQL Injection Check
+    				// Check password/username
+    				// If match
+    				setCookie(HttpCookie(cookieName, "content")) {
+	    				complete {
+	    					// Redirect to appropiate page
+	    					""
+	    				}
+    				}
+    				// If not match
+    				// 403
+    			}
+    		}
+   		}
+   	}
 }
