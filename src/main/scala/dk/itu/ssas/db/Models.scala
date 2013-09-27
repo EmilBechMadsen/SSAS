8e1199264d30036b5d7a4a3dda4f5e39b8e2efb1
Christian Harrington <christian@harrington.dk>
2013-09-27 12:02:25 +0200

    Most of the user model is now implemented

--- src/main/scala/dk/itu/ssas/db/Models.scala
+++ src/main/scala/dk/itu/ssas/db/Models.scala
@@ -2,102 +2,118 @@ package dk.itu.ssas.db
 
 trait DbModels {
   import scala.slick.driver.MySQLDriver.simple._
+  import scala.slick.lifted.ForeignKeyAction
   import java.util.UUID
 
-  import dk.itu.ssas.model.User
+  import dk.itu.ssas.model._
 
-  object Users extends Table[User]("USER") {
-    def id       = column[Int]("ID", O.PrimaryKey, O.AutoInc) // This is the primary key column
-    def name     = column[String]("NAME")
-    def address  = column[Option[String]]("ADDRESS")
-    def email    = column[String]("EMAIL")
-    def password = column[String]("PASSWORD")
-    def salt     = column[String]("SALT")
+  implicit val relationshipTypeMapper = MappedTypeMapper.base[Relationship, String](
+    { r =>
+      r.toString()
+    }, 
+    { s =>
+      Relationship(s)
+    }
+  )
 
-    def * = id ~ name ~ address ~ email ~ password ~ salt <> ((id, name, address, email, password, salt) => User.apply(id, name, address, email, password, salt), User.unapply _)
+  object Users extends Table[User]("user") {
+    def id       = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name     = column[String]("name", O.NotNull)
+    def address  = column[Option[String]]("address")
+    def email    = column[String]("email", O.NotNull)
+    def password = column[String]("password", O.NotNull)
+    def salt     = column[String]("salt", O.NotNull)
+
+    def * = id ~ name ~ address ~ email ~ password ~ salt <> ((id, name, address, email, password, salt) => User.apply(id, name, address, email, password, salt), User unapply _) 
     def forInsert = name ~ address ~ email ~ password ~ salt
+
+    def uniqueEmail = index("idx_email", email)
   }
 
-  case class Friend(user1Id: Int, user2Id: Int, relationship: String)
+  case class Friend(user1Id: Int, user2Id: Int, relationship: Relationship)
 
-  object Friends extends Table[Friend]("FRIEND") {
-    def user1Id      = column[Int]("USER1")
-    def user2Id      = column[Int]("USER2")
-    def relationship = column[String]("RELATIONSHIP")
+  object Friends extends Table[Friend]("friend") {
+    def user1Id      = column[Int]("user1")
+    def user2Id      = column[Int]("user2")
+    def relationship = column[Relationship]("relationship", O.NotNull, O.Default(Friendship))
 
     def * = user1Id ~ user2Id ~ relationship <> (Friend, Friend unapply _)
 
-    def user1        = foreignKey("FK_FRIEND_USER1", user1Id, Users)(_.id)
-    def user2        = foreignKey("FK_FRIEND_USER2", user2Id, Users)(_.id)
+    def user1        = foreignKey("fk_friend_user1", user1Id, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
+    def user2        = foreignKey("fk_friend_user2", user2Id, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
 
-    def pk           = primaryKey("pk_FRIEND", (user1Id, user2Id))
+    def pk           = primaryKey("pk_friend", (user1Id, user2Id))
   }
 
-  object Admins extends Table[(Int)]("ADMIN") {
-    def userId       = column[Int]("USER", O.PrimaryKey)
+  object Admins extends Table[(Int)]("admin") {
+    def userId       = column[Int]("user", O.PrimaryKey)
 
     def * = userId
 
-    def user = foreignKey("FK_ADMIN_USER", userId, Users)(_.id)
+    def user = foreignKey("fk_admin_user", userId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
   }
 
   case class Session(key: UUID, userId: Int)
 
-  object Sessions extends Table [Session]("SESSION") {
-    def key          = column[UUID]("SESSION_KEY", O.PrimaryKey)
-    def userId       = column[Int]("USER")
+  object Sessions extends Table [Session]("session") {
+    def key          = column[UUID]("session_key", O.PrimaryKey)
+    def userId       = column[Int]("user", O.NotNull)
 
     def * = key ~ userId <> (Session, Session unapply _)
 
-    def user         = foreignKey("FK_SESSION_USER", userId, Users)(_.id)
+    def user         = foreignKey("fk_session_user", userId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
   }
 
-  case class FriendRequest(fromUserId: Int, toUserId: Int, relationship: String)
+  case class FriendRequest(fromUserId: Int, toUserId: Int, relationship: Relationship)
 
-  object FriendRequests extends Table [FriendRequest]("FRIEND_REQUEST") {
-    def fromUserId   = column[Int]("FROM_USER")
-    def toUserId     = column[Int]("TO_USER")
-    def relationship = column[String]("RELATIONSHIP")
+  object FriendRequests extends Table [FriendRequest]("friend_request") {
+    def fromUserId   = column[Int]("from_user", O.NotNull)
+    def toUserId     = column[Int]("to_user", O.NotNull)
+    def relationship = column[Relationship]("relationship", O.NotNull, O.Default(Friendship))
 
     def * = fromUserId ~ toUserId ~ relationship <> (FriendRequest, FriendRequest unapply _)
 
-    def fromUser     = foreignKey("FK_FRIEND_REQUEST_USER1", fromUserId, Users)(_.id)
-    def toUser       = foreignKey("FK_FRIEND_REQUEST_USER2", toUserId, Users)(_.id)
+    def fromUser     = foreignKey("fk_friend_request_user1", fromUserId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
+    def toUser       = foreignKey("fk_friend_request_user2", toUserId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
 
-    def pk           = primaryKey("pk_FRIEND_REQUEST", (fromUserId, toUserId))
+    def pk           = primaryKey("pk_friend_request", (fromUserId, toUserId))
   }
 
   case class EmailConfirmation(key: UUID, userId: Int)
 
-  object EmailConfirmations extends Table [EmailConfirmation]("SESSION") {
-    def guid         = column[UUID]("GUID", O.PrimaryKey)
-    def userId       = column[Int]("USER")
+  object EmailConfirmations extends Table [EmailConfirmation]("email_confirmation") {
+    def guid         = column[UUID]("guid", O.PrimaryKey)
+    def userId       = column[Int]("user", O.NotNull)
 
     def * = guid ~ userId <> (EmailConfirmation, EmailConfirmation unapply _)
 
-    def user         = foreignKey("FK_EMAIL_CONFIRMATION_USER", userId, Users)(_.id)
+    def user         = foreignKey("fk_email_confirmation_user", userId, Users)(_.id,onDelete = ForeignKeyAction.Cascade)
   }
 
   case class Hobby(id: Int, name: String)
 
-  object Hobbies extends Table[Hobby]("HOBBY") {
-    def id           = column[Int]("ID", O.PrimaryKey, O.AutoInc)
-    def name         = column[String]("NAME")
+  object Hobbies extends Table[Hobby]("hobby") {
+    def id           = column[Int]("id", O.PrimaryKey, O.AutoInc)
+    def name         = column[String]("name", O.NotNull)
 
     def * = id ~ name <> (Hobby, Hobby unapply _)
   }
 
   case class UserHobby(userId: Int, hobbyId: Int)
 
-  object UserHobbies extends Table[UserHobby]("USER_HOBBY") {
-    def userId       = column[Int]("USER")
-    def hobbyId      = column[Int]("HOBBY")
+  object UserHobbies extends Table[UserHobby]("user_hobby") {
+    def userId       = column[Int]("user", O.NotNull)
+    def hobbyId      = column[Int]("hobby", O.NotNull)
 
     def * = userId ~ hobbyId <> (UserHobby, UserHobby unapply _)
 
-    def user         = foreignKey("FK_USER_HOBBY_USER", userId, Users)(_.id)
-    def hobby        = foreignKey("FK_USER_HOBBY_HOBBY", hobbyId, Hobbies)(_.id)
+    def user         = foreignKey("fk_user_hobby_user", userId, Users)(_.id, onDelete = ForeignKeyAction.Cascade)
+    def hobby        = foreignKey("fk_user_hobby_hobby", hobbyId, Hobbies)(_.id, onDelete = ForeignKeyAction.Cascade)
 
-    def pk           = primaryKey("pk_FRIEND_REQUEST", (userId, hobbyId))
+    def pk           = primaryKey("pk_friend_request", (userId, hobbyId))
   }
+
+  val ddl = Users.ddl ++ Friends.ddl ++ Admins.ddl ++ Sessions.ddl ++ 
+            FriendRequests.ddl ++ EmailConfirmations.ddl ++ 
+            Hobbies.ddl ++ UserHobbies.ddl
 }
--- src/main/scala/dk/itu/ssas/model/User.scala
+++ src/main/scala/dk/itu/ssas/model/User.scala
@@ -16,16 +16,27 @@ object User extends DbModels {
     * @param password - The users password
     * @return Maybe a user
     */
-  def apply(email: String, password: String): Option[User] = {
+  def login(email: String, password: String): Option[User] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     val user = (for (u <- Users if u.email === email) yield u) firstOption
 
     user match {
       case Some(u) => Security.checkPassword(password, u.password, u.salt) match {
-        case true  => user
+        case true  => {
+          val q = (for (s <- Sessions if s.userId === u.id) yield s)
+
+          val s = Session(UUID.randomUUID(), u.id)
+
+          q firstOption match {
+            case Some(s) => q update s
+            case None    => Sessions insert s
+          }
+
+          user
+        }
         case false => None
       }
       case None => None
-    }   
+    }
   }
 
   /** Returns a user if the id exists
@@ -65,7 +76,7 @@ object User extends DbModels {
     * @param password - The user's password
     * @return The created user
     */
-  def createUser(name: String, address: Option[String], email: String, password: String): Option[User] = {
+  def create(name: String, address: Option[String], email: String, password: String): Option[User] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     val (hashedPw, salt) = Security.newPassword(password)
     val id = Users.forInsert returning Users.id insert (name, address, email, hashedPw, salt)
 
@@ -81,37 +92,46 @@ case class User(
       val password: String,
       val salt: String)
     extends DbModels {
+  def logout(): Unit = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
+    val q = (for (s <- Sessions if s.userId === id) yield s)
+
+    q firstOption match {
+      case Some(s) => q delete
+      case None    => {}
+    }
+  }
+
   /** Returns the user's friends
     *
     * @return A map from the relationship to the user
     */
-  def friends: Map[Relationship, User] = {
+  def friends: Map[Relationship, User] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     val fs = for {
       f <- Friends if  f.user1Id === id   || f.user2Id === id
       u <- Users   if (f.user1Id === u.id || f.user2Id === u.id) && !(u.id === id)
     } yield (f.relationship, u)
 
-    (for ((r, u) <- fs.list) yield Relationship(r) -> u) toMap
+    (for ((r, u) <- fs.list) yield r -> u) toMap
   }
 
   /** Returns the outstanding friend requests for the user
     *
     * @return A map from the relationship to the user
     */
-  def friendRequests: Map[Relationship, User] = {
+  def friendRequests: Map[Relationship, User] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     val fs = for {
       f <- FriendRequests if f.toUserId   === id
       u <- Users          if f.fromUserId === u.id
     } yield (f.relationship, u)
 
-    (for ((r, u) <- fs.list) yield Relationship(r) -> u) toMap
+    (for ((r, u) <- fs.list) yield r -> u) toMap
   }
 
   /** Returns the a list of the users hobbies
     *
     * @return A list of the user's hobbies
     */
-  def hobbies: List[Hobby] = {
+  def hobbies: List[Hobby] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     val hs = for {
       uh <- UserHobbies if uh.userId  === id
       h  <- Hobbies     if uh.hobbyId === h.id
@@ -124,7 +144,33 @@ case class User(
     *
     * @return Returns true if the user is an admin, false otherwise
     */
-  def admin: Boolean = {
+  def admin: Boolean = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
     (for (a <- Admins if (a.userId === id)) yield a.userId).list.nonEmpty
+  }
+
+  def admin_=(admin: Boolean): Unit = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
+    val q = (for (a <- Admins if a.userId === id) yield a)
+
+    (q firstOption, admin) match {
+      case (Some(a), false) => q delete
+      case (None,    true)  => Admins insert id
+      case _                => {}
+    }
+  } 
+
+  def email_=(email: String): Option[User] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
+    val q = for (u <- Users if u.id === id) yield (u.email)
+
+    q update email
+
+    (for (u <- Users if u.id === id && u.email === email) yield u) firstOption
+  }
+
+  def session: Option[UUID] = Database.forURL(Settings.dbString, driver = Settings.dbDriver) withSession {
+    (for (s <- Sessions if s.userId === id) yield s.key) firstOption
+  }
+
+  override def toString(): String = {
+    s"User($id, $name, $address, $email)"
   }
 }
