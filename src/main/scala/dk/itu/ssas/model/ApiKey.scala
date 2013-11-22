49b6a4a2bd5c3c2741f94ecd2304835827fed76d
Christian Harrington <christian@harrington.dk>
2013-11-22 10:33:30 +0100

    Added methods to ApiKey model to create and revoke API keys.

--- src/main/scala/dk/itu/ssas/model/ApiKey.scala
+++ src/main/scala/dk/itu/ssas/model/ApiKey.scala
@@ -7,11 +7,42 @@ import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.driver.MySQLDriver.simple.Database.threadLocalSession
 
 object ApiKey extends DbAccess {
+  /** Retrieves an API key from the database.
+    *
+    * @param key - The key of the desired API key.
+    * @return An API key, if it exists.
+    */
   def apply(key: UUID): Option[ApiKey] = Db withSession {
     (for (a <- ApiKeys if a.key === key.toString()) yield a) firstOption
   }
+
+  /** Creates a new API key in the database.
+    *
+    * @return The new API key.
+    */
+  def create(): ApiKey = Db withSession {
+    val apiKey = ApiKey(UUID.randomUUID().toString(), false)
+    ApiKeys insert apiKey
+
+    apiKey
+  }
 }
 
-case class ApiKey(private val _key: String, revoked: Boolean) {
+case class ApiKey(private val _key: String, private var _revoked: Boolean) extends DbAccess {
   val key = UUID.fromString(_key)
+
+  /** The status of the API key.
+    *
+    * @return True if the API key has been revoked, false otherwise.
+    */
+  def revoked: Boolean = _revoked
+
+  /** Sets the status of the API key.
+    *
+    * @param revoke - The status to set.
+    */
+  def revoked_=(revoke: Boolean): Unit = Db withSession {
+    (for (a <- ApiKeys if a.key === _key) yield a.revoked) update revoke
+    _revoked = revoke
+  }
 }
